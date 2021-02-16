a99ab6bfb8a83c716662d7fa72ff1b90afbcc0df
Abigail Adams <abigail-adams@gmail.com>
2021-02-16 12:50:09 -0800

    adding all files

---
+++ assets/javascript/script.js
@@ -0,0 +1,9 @@
+// add and remove class -- similar to hide 
+// Include for past/pres./future
+// grab current hour from moment.js
+
+// Getting the date to show up - google
+// Use moment.js for the time 
+    // needed for the current date 
+
+// saving the textareas to local storage and redisplaying when page is refreshed
