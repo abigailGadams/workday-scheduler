7f89597131c5b6c3dd31e591b1f9ea35bb207885
Abigail Adams <abigail-adams@gmail.com>
2021-02-17 12:32:58 -0800

    add color code for time blocks

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,5 +5,23 @@
 // Getting the date to show up - google
 // Use moment.js for the time 
     // needed for the current date 
-
+var currentHour = moment().hour();
+var militaryHour = [9,10,11,12,13,14,15,16,17];
+console.log(currentHour);
+ 
+for (var i=0; i<9; i++){
+    if (currentHour=== militaryHour[i]){
+         console.log(i);
+        document.getElementById(i).classList.add("present")
+    }
+    if (currentHour> militaryHour[i]){
+        console.log(i);
+       document.getElementById(i).classList.add("past")
+   }
+   if (currentHour< militaryHour[i]){
+    console.log(i);
+   document.getElementById(i).classList.add("future")
+}
+ 
+}
 // saving the textareas to local storage and redisplaying when page is refreshed
